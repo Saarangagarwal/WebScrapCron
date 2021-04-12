a94a9e9b65565d60ca318a25ad4e4d0dd407b6af
Saarang Agarwal <saarang.agarwal@sscinc.com>
2021-04-12 11:54:33 -0400

    enhanced cron

--- index.js
+++ index.js
@@ -35,22 +35,115 @@ let value2;
 async function puppet () {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    //await page.goto('https://www.walmart.ca/en/ip/mrnoodles-vegetable-flavoured-instant-noodles/6000153706380');
-    //await page.goto('https://www.instacart.ca/');
-    //await page.type('input[name="nextgen-authenticate.all.postal_code"]', 'N2L3B8') ;
 
-    //await page.click('button[data-var="loginBtn"]');
-    //await page.click('[name="button"]')
-    //document.querySelector("#root > div > div.css-l6ct9h > div.css-1wfm2tp > div.css-8atqhb > form > p > span > button")
-    
-    //let form = await page.$('#root > div > div.css-l6ct9h > div.css-1wfm2tp > div.css-8atqhb > form > p > span > button');
-        // await page.evaluate(() => {
-        //    document.querySelector("#root > div > div.css-l6ct9h > div.css-1wfm2tp > div.css-8atqhb > form > p > span > button").click() 
-        // }
-        // );
-    
+    // //kya80332@eoopy.com
+    // //insta@qwerty123walmart
+    // await page.goto('https://www.instacart.ca/');
+    // //await page.type('input[name="nextgen-authenticate.all.postal_code"]', 'N2L3B8') ;
 
-    //await page.waitForNavigation()
+    // //await page.click('button[data-var="loginBtn"]');
+    // //await page.click('[name="button"]')
+    
+    // const form = await page.$('#root > div > div.css-l6ct9h > div.css-1wfm2tp > div.css-8atqhb > form > p > span > button');
+    // await form.evaluate( form => form.click() );
+
+    // await page.type('input[name="nextgen-authenticate\.all\.log_in_email"]', 'kya80332@eoopy.com') ;
+    // await page.type('input[name="nextgen-authenticate\.all\.log_in_password"]', 'insta@qwerty123walmart') ;
+
+    // const form1 = await page.$('#root > div > div.css-l6ct9h > div.css-1wfm2tp > div.css-8atqhb > form > div:nth-child(6) > button');
+    // //const form1 = await page.$('#root > div > div.css-l6ct9h > div.css-1wfm2tp > div.css-8atqhb > form > div:nth-child(7) > button');
+    // await form1.evaluate( form1 => form1.click() );
+
+
+    // // var time = 0;
+
+    // // const timer =  setInterval(function() {
+    // //     time += 1;
+    // //     console.log(time + " seconds have passed");
+    // //     if (time > 5) {
+    // //         clearInterval(timer);
+    // //     }
+    // // }, 2000);
+    
+     
+    // // await page.waitForNavigation()
+    // //await page._frameManager._mainFrame.waitForNavigation();
+    // await page.screenshot({path: 'test.png'});
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+    // await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
+
+    // await page.goto('https://www.instacart.ca/store/items/item_792008452');
+    // await page.waitForSelector('#react-views-container > div > div > div:nth-child(1) > div > div > div > div > div:nth-child(1) > div > div.col-md-5.itemModalHeader > div:nth-child(1) > div:nth-child(3) > div > div.item-price > span:nth-child(2)')
+    // element = await page.$('#react-views-container > div > div > div:nth-child(1) > div > div > div > div > div:nth-child(1) > div > div.col-md-5.itemModalHeader > div:nth-child(1) > div:nth-child(3) > div > div.item-price > span:nth-child(2)')
+    // value2 = await page.evaluate(el => el.textContent, element)
+    // console.log("instacart: "+value2)
+    // await page.screenshot({path: 'test1.png'});
 
     await page.goto('https://www.lenovo.com/ca/en/laptops/thinkpad/thinkpad-t-series/ThinkPad-T14-G1/p/20S0004SUS');
     await page.waitForSelector('#longscroll-subseries > div.backgroundColor.subseriesHeader.noeSpot > div.hero-column.hero-column-one > div.cta > div.cta-group-price > dl > dd')
